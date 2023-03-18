9a4a34d5974320245a213ef3199cad2912e4a934
Brandon <brandon@onlinehome.de>
2023-03-18 08:03:12 +0000

    feat: huli huli chicken

--- aloha_team_sai/pages/99_🍱_Food.py
+++ aloha_team_sai/pages/99_🍱_Food.py
@@ -3,10 +3,12 @@ from typing import Optional
 
 from Home import meta_info
 
-def post(path_text:str, path_picture:Optional[str] = None, caption: Optional[str] = None) -> None:
+def post(path_text:str, path_picture:Optional[str] = None, caption: Optional[str] = None, title: Optional[str] = None) -> None:
     with open(file=path_text, mode="r") as file:
         text = file.read()
-    title = path_text.split("_")[1].split(".")[0].capitalize()
+
+    if title is None:
+        title = path_text.split("_")[1].split(".")[0].capitalize()
     st.subheader(body=title)
     if not path_picture is None:
         st.image(image=path_picture, caption=caption)
@@ -36,6 +38,15 @@ def main():
             path_text="texts/food_früchte.md", 
             path_picture="images/food_hawaiian_fruits.jpg", 
             caption="Hawaiian Fruits.")
+    
+    with st.expander(label="Hawaiianische Gerichten"):
+        post(
+            path_text="texts/food_huli_huli_chicken.md",
+            path_picture="images/huli_huli_chicken.jpg",
+            caption="Some fine plate with huli huli chicken",
+            title="Huli Huli Chicken")
+
+    
     return 
 
 if __name__ == "__main__":
---
+++ aloha_team_sai/texts/food_huli_huli_chicken.md
@@ -0,0 +1,12 @@
+Obwohl Hühnchen nicht wie ein Gericht klingt, das einzigartig für Hawaii ist, sollte man unbedingt Huli Huli Chicken probieren, wenn man Hawaii besucht.
+
+"Huli" bedeutet in Hawaiianisch drehen und genau so wird das Gericht zubereitet. Das Hühnchen wird mit einer süßen Sauce überzogen und dann über einem Grill gedreht. Es wird am besten über Kiawe-Holz, das in Hawaii heimisch ist, gegrillt.
+
+Das Gericht ist köstlich und wird von Einheimischen und Touristen gleichermaßen genossen. Viele Einheimische machen es gerne für Fundraiser und Schulveranstaltungen, bei denen viele Menschen versorgt werden müssen.
+
+    Winna, Winna - Chicken Dinna!
+
+**Wo könnt ihr hingehen:**
+- [Mike's Huli Chicken](https://www.mikeshulichicken.com/)
+- [Maui Mike's Fire Roasted Chicken](https://mauimikes.com/)
+- [Da Mexican Huli Huli](https://kapoleifoodtruck.com/)
