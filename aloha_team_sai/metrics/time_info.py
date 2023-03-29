a19916803d41ef841dfba317f51007393806e6b1
Brandon <brandon@onlinehome.de>
2023-03-29 16:32:02 +0000

    feat: time bar

---
+++ aloha_team_sai/metrics/time_info.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from datetime import datetime, timedelta
+import pytz
+
+# Define the timezones for Honolulu and Stuttgart
+HNL_TZ = pytz.timezone('Pacific/Honolulu')
+STR_TZ = pytz.timezone('Europe/Berlin')
+
+def TimeBar():
+    # Get the current time in Honolulu and Stuttgart
+    honolulu_time = datetime.now(HNL_TZ).strftime("%d.%b, %H:%M")
+    stuttgart_time = datetime.now(STR_TZ).strftime("%d.%b, %H:%M")
+
+    # Display the time in two separate Metric Elements
+    col1, col2 = st.columns(2)
+    col1.metric("Honolulu", honolulu_time)
+    col2.metric("Stuttgart", stuttgart_time)
+    return 
