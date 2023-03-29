4d46df4b1eb08e8372556fbbf6957b6648dc9894
Brandon <brandon@onlinehome.de>
2023-03-29 17:04:16 +0000

    refactor: order of information

--- aloha_team_sai/metrics/time_info.py
+++ aloha_team_sai/metrics/time_info.py
@@ -8,11 +8,11 @@ STR_TZ = pytz.timezone('Europe/Berlin')
 
 def TimeBar():
     # Get the current time in Honolulu and Stuttgart
-    honolulu_time = datetime.now(HNL_TZ).strftime("%d.%b, %H:%M")
     stuttgart_time = datetime.now(STR_TZ).strftime("%d.%b, %H:%M")
+    honolulu_time = datetime.now(HNL_TZ).strftime("%d.%b, %H:%M")
 
     # Display the time in two separate Metric Elements
     col1, col2 = st.columns(2)
-    col1.metric("Honolulu", honolulu_time)
-    col2.metric("Stuttgart", stuttgart_time)
+    col1.metric("Stuttgart", stuttgart_time)
+    col2.metric("Honolulu", honolulu_time)
     return 
